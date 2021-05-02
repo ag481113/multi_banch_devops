ebd425fd61517be4ed95a8dfb7d2f3a12bb01e21
ag481113 <ag481113@gmail.com>
2021-05-02 10:06:09 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -7,8 +7,8 @@ def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t aditya1035/demo-app:jma-2.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push aditya1035/demo-app:jma-2.0'
+        //sh "echo $PASS | docker login -u $USER --password-stdin"
+        //sh 'docker push aditya1035/demo-app:jma-2.0'
     }
 } 
 
