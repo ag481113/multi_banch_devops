a6f3f4301348b9d93c727df0ba4f5d39e4313964
ag481113 <ag481113@gmail.com>
2021-09-28 08:59:08 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -5,7 +5,7 @@ def buildJar() {
 
 def buildImage() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-pwd-in-jenkins', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t aditya1035/demo-app:jma-3.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh 'docker push aditya1035/demo-app:jma-3.0'
